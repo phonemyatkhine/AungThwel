44bd7e7f208c95ee63ea105e22e263cf44eba95a
phonemyatkhine <37993997+phonemyatkhine@users.noreply.github.com>
2018-04-02 11:21:21 +0630

    Add files via upload

---
+++ AungThwel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aung.thwel;
+
+/**
+ *
+ * @author GL62M 7RDX
+ */
+public class AungThwel {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.lang.InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        
+        StartLogo s = new StartLogo();
+    }
+    
+}
